a1abf0159fb69ec5ccadccc263719273f66d1fea
John Ric Merque <johnricmer@gmail.com>
2022-11-12 11:29:34 +0800

    displayed menu options

--- program.py
+++ program.py
@@ -23,4 +23,11 @@
 # 2. create navigation to commands based on the input
 # 3. for command 1, create a series of user input and store details in a dictionary
 # 4. for command 2, access the dictionary and display dictionary values
-# 5. for command 3, enclose the everuthing in a loop and add break statement to terminate
+# 5. for command 3, enclose everything in a loop and add break statement to terminate
+
+
+print("\n~~~~~~Menu:~~~~~~")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
+print("~~~~~~~~~~~~~~~~~\n")
