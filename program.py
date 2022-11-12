881de6f5f8afef10c88d17adb7ef81a510a15b93
John Ric Merque <johnricmer@gmail.com>
2022-11-12 11:54:45 +0800

    created if else to navigate command based on user input

--- program.py
+++ program.py
@@ -25,15 +25,30 @@ print("2 -> Search")
 print("3 -> Exit (y/n)")
 print("~~~~~~~~~~~~~~~~~\n")
 
+userCommandChoice = ""
+
 # 2. ask user input for option selected
 try:
     userCommandChoice = int(input("Please select a command option (1-3): "))
 except:
     print("Invalid input. Make sure to enter a number from (1-3)")
-# 2. create navigation to commands based on the input
-# 3. for command 1, create a series of user input and store details in a dictionary
-# 4. for command 2, access the dictionary and display dictionary values
-# 5. for command 3, enclose everything in a loop and add break statement to terminate
+
+# 3. create navigation to commands based on the input
+if userCommandChoice == 1:
+    # define a function to add items
+    print("adds items") 
+elif userCommandChoice == 2:
+    # define a function to search items
+    print("searches")
+elif userCommandChoice == 3:
+    # define a function to exit 
+    print("exits")
+else:
+    print("Command does not exist")
+
+# 4. for command 1, create a series of user input and store details in a dictionary
+# 5. for command 2, access the dictionary and display dictionary values
+# 6. for command 3, enclose everything in a loop and add break statement to terminate
 
 
 
