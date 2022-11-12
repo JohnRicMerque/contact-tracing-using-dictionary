6a12e3a6ae2da333fe3b09256c8bf3d61d965b62
John Ric Merque <johnricmer@gmail.com>
2022-11-12 11:44:05 +0800

    asked user input for command choice and validated input

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-# John Ric Merque | BSCOE 2-6 | 
+# John Ric Merque | BSCOE 2-6 | DSA
 
 # Write a python program for contact tracing:
 
@@ -19,15 +19,22 @@
 # PseudoCode
  
 # 1. display menu
+print("\n~~~~~~Menu:~~~~~~")
+print("1 -> Add an item")
+print("2 -> Search")
+print("3 -> Exit (y/n)")
+print("~~~~~~~~~~~~~~~~~\n")
+
 # 2. ask user input for option selected
+try:
+    userCommandChoice = int(input("Please select a command option (1-3): "))
+except:
+    print("Invalid input. Make sure to enter a number from (1-3)")
 # 2. create navigation to commands based on the input
 # 3. for command 1, create a series of user input and store details in a dictionary
 # 4. for command 2, access the dictionary and display dictionary values
 # 5. for command 3, enclose everything in a loop and add break statement to terminate
 
 
-print("\n~~~~~~Menu:~~~~~~")
-print("1 -> Add an item")
-print("2 -> Search")
-print("3 -> Exit (y/n)")
-print("~~~~~~~~~~~~~~~~~\n")
+
+
